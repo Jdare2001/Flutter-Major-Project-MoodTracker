c2edd2ef2d91c52e2de430a908fc37bbd5ac1f2b
Jayden <59065556+Jdare2001@users.noreply.github.com>
2024-01-31 11:57:10 +0000

    added percentage indicator for habits

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:percent_indicator/percent_indicator.dart';
 import 'package:moodtracker/utilities/topAppBar.dart';
 
 class HomePage extends StatefulWidget {
@@ -14,7 +14,51 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: getTopAppBar("Welcome Back", context),
-      body: Text("Home Screen"),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 100),
+            Container(
+              height: 200,
+              width: 380,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50),
+                color: Colors.lightBlue.shade200,
+              ),
+              child: Row(
+                children: [
+                  const SizedBox(
+                    width: 25,
+                  ),
+                  CircularPercentIndicator(
+                    radius: 50,
+                    backgroundColor: Colors.white,
+                    percent: 0.74,
+                    progressColor: Colors.deepPurple,
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  const Column(
+                    children: [
+                      SizedBox(
+                        height: 55,
+                      ),
+                      Text(
+                        'You have completed',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                      Text('70%', style: TextStyle(fontSize: 25)),
+                      Text('of your habits for the day',
+                          style: TextStyle(fontSize: 18)),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
